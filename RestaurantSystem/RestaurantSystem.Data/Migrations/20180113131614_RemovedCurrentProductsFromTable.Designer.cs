8af071d5fd6f794f4950f1057401c83283c51a7e
badbutcher <pavel.p.n@abv.bg>
2018-01-13 15:17:41 +0200

    Adding products now works

--- RestaurantSystem/RestaurantSystem.Data/Migrations/20180113131614_RemovedCurrentProductsFromTable.Designer.cs
+++ RestaurantSystem/RestaurantSystem.Data/Migrations/20180113131614_RemovedCurrentProductsFromTable.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace RestaurantSystem.Data.Migrations
 {
     [DbContext(typeof(RestaurantSystemDbContext))]
-    partial class RestaurantSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180113131614_RemovedCurrentProductsFromTable")]
+    partial class RemovedCurrentProductsFromTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
